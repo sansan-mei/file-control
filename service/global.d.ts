42e7bfd1cb3d6e504c7d460671aca8158d6ca1e7
MEI <1596944197@qq.com>
2024-04-02 17:21:07 +0800

    chore: 多余文件删除

--- service/global.d.ts
+++ service/global.d.ts
@@ -2,10 +2,6 @@ declare global {
   namespace NodeJS {
     interface ProcessEnv {
       NODE_ENV: 'development' | 'production'
-      PORT: string
-      TEED: string | undefined
-      HASH_SALT: string | undefined
-      OPENAI_API_BASE_URL: string | undefined
     }
   }
 }
