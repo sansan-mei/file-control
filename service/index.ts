f1bd468695133f65861f86f4a30c5553acc3d016
梅花三十三 <1596944197@qq.com>
2024-10-09 16:48:30 +0800

    feat: 增加直链访问参数

--- service/index.ts
+++ service/index.ts
@@ -1,9 +1,9 @@
-import express from 'express'
-import cookies from 'cookie-parser'
-import "#src/redis"
 import '#src/api'
+import '#src/redis'
 import { renderRoutes } from '#src/utils'
+import cookies from 'cookie-parser'
 import cors from 'cors'
+import express from 'express'
 
 process.on('unhandledRejection', (reason, promise) => {
   console.error('未处理的异步err:', promise, 'reason:', reason)
@@ -14,23 +14,25 @@ process.on('uncaughtException', (error) => {
 
 const app = express()
 const router = express.Router()
-app.use(cors({
-  credentials: true,
-  origin: process.env.ALLOW_ORIGIN || '*'
-}))
+app.use(
+  cors({
+    credentials: true,
+    origin: process.env.ALLOW_ORIGIN || '*'
+  })
+)
 app.use(cookies())
 app.set('trust proxy', true)
 
-app.use(express.static('public', {
-  maxAge: '12h',
-  immutable: true,
-}))
+app.use(
+  express.static('public', {
+    maxAge: '12h',
+    immutable: true
+  })
+)
 app.use(router)
 
 renderRoutes(router)
 
 app.listen(3003, () => {
-  console.log('Server is running on port 3003');
+  console.log('Server is running on port 3003')
 })
-
-
--- service/src/api/index.ts
+++ service/src/api/index.ts
@@ -1,12 +1,12 @@
+import { client } from '#src/redis'
 import { createReadStream, statSync } from 'fs'
-import { basename, join } from 'path'
 import { unlink } from 'fs/promises'
 import mime from 'mime'
+import multer from 'multer'
+import { basename, join } from 'path'
 import { ADMIN, NOT_SHOW_PASS, expireTime } from 'src/constant'
 import { uploadInstance } from 'src/middleware/uploadInstance'
 import { $api, FileApi, catchError, getDirectoryTree, getFolderSize } from 'src/utils'
-import multer from 'multer'
-import { client } from '#src/redis'
 
 /** @其他常量 */
 const uploadList: string[] = []
@@ -26,72 +26,77 @@ $api.add([
         res.setHeader('Cache-Control', 'max-age=1') // 缓存响应
         return { data: dirTree }
       })
-    },
+    }
   },
   // /check-file
   {
-
-    path: '/check-file/:hash/:filename',
+    path: '/check-file/:hash/:filename/:password?',
     async handler(req, res) {
       try {
-        const hash = req.params.hash;
-        const filename = req.params.filename;
-        const { token, filePwd } = req.cookies;
-        if (req.headers['if-none-match'] === hash) {
-          res.setHeader('cache-control', `max-age=${expireTime},public`); // 缓存响应
-          res.status(304).end();
-          return;
+        const hash = req.params.hash
+        const filename = req.params.filename
+        const filePwd = req.params.password || req.cookies.filePwd
+        const { token } = req.cookies
+
+        const path = FileApi.getPath(hash)
+        const type = mime.getType(path)
+        if (type) {
+          res.setHeader('content-type', `${type}; charset=utf-8`) // 确保添加了字符编码
         }
-        const admin = await client.get(ADMIN);
-        const path = FileApi.getPath(hash);
-        const stat = statSync(path);
-        const prefixFilename = basename(path);
-        const prefix = prefixFilename.slice(0, prefixFilename.indexOf('-'));
+
+        if (req.headers['if-none-match'] === hash) {
+          res.setHeader('cache-control', `max-age=${expireTime},public`) // 缓存响应
+          res.status(304).end()
+          return
+        }
+        const admin = await client.get(ADMIN)
+        const stat = statSync(path)
+        const prefixFilename = basename(path)
+        const prefix = prefixFilename.slice(0, prefixFilename.indexOf('-'))
         /** @description:验证文件密码是否跟提交的时候一致 */
         if (token !== admin && !path.includes(NOT_SHOW_PASS)) {
           if (prefix.includes('#')) {
-            const pwd = prefix.slice(0, prefix.indexOf('#'));
-            if (filePwd !== pwd) return res.status(403).end('密码错误');
+            const pwd = prefix.slice(0, prefix.indexOf('#'))
+            if (filePwd !== pwd) {
+              res.setHeader('content-type', 'text/html; charset=utf-8')
+              return res.status(403).end('密码错误')
+            }
           }
         }
 
-        const type = mime.getType(path);
-        if (type) {
-          res.setHeader('content-type', `${type}; charset=utf-8`); // 确保添加了字符编码
-        }
-
-        res.setHeader('cache-control', `max-age=${expireTime},public`); // 缓存响应
-        res.setHeader('ETag', hash); // 缓存响应
+        res.setHeader('cache-control', `max-age=${expireTime},public`) // 缓存响应
+        res.setHeader('ETag', hash) // 缓存响应
 
         // Range support
-        const range = req.headers.range;
+        const range = req.headers.range
         if (range) {
-          const positions = range.replace(/bytes=/, "").split("-");
-          const start = parseInt(positions[0], 10);
-          const total = stat.size;
-          const end = positions[1] ? parseInt(positions[1], 10) : total - 1;
-          const chunksize = (end - start) + 1;
+          const positions = range.replace(/bytes=/, '').split('-')
+          const start = parseInt(positions[0], 10)
+          const total = stat.size
+          const end = positions[1] ? parseInt(positions[1], 10) : total - 1
+          const chunksize = end - start + 1
 
           res.writeHead(206, {
             'Content-Range': `bytes ${start}-${end}/${total}`,
             'Accept-Ranges': 'bytes',
-            'Content-Length': chunksize,
-          });
+            'Content-Length': chunksize
+          })
 
-          const readStream = createReadStream(path, { start, end });
-          readStream.on('error', console.error);
-          readStream.pipe(res);
+          const readStream = createReadStream(path, { start, end })
+          readStream.on('error', console.error)
+          readStream.pipe(res)
         } else {
-          res.setHeader('Content-Length', stat.size); // 添加文件大小到响应头
-          filename && res.setHeader('Content-Disposition', `inline; filename=${encodeURIComponent(filename)}`); // 设置文件名
-          const readStream = createReadStream(path);
-          readStream.on('error', console.error);
-          readStream.pipe(res);
+          res.setHeader('Content-Length', stat.size) // 添加文件大小到响应头
+          filename &&
+            res.setHeader('Content-Disposition', `inline; filename=${encodeURIComponent(filename)}`) // 设置文件名
+          const readStream = createReadStream(path)
+          readStream.on('error', console.error)
+          readStream.pipe(res)
         }
       } catch (error) {
-        console.error(error);
-        res.status(400);
-        res.end('状态异常');
+        console.error(error)
+        res.status(400)
+        res.end('状态异常')
       }
     }
   },
@@ -105,57 +110,55 @@ $api.add([
         uploadList.shift()
         return { data: null, message: '上传成功' }
       })
-    },
+    }
   },
   // /check-memory
   {
-
     path: '/check-memory',
     handler(req, res) {
       catchError(res, async () => {
         const size = req.query.size as string
         const folderSize = getFolderSize(pathTo)
         // 单位为mb
-        if (+size + folderSize >= 20720)
-          throw new Error('占用超出上限')
-        else if (uploadList.length >= 5)
-          throw new Error('上传队列已满')
-
-          ; +size >= 20 && uploadList.push(size)
+        if (+size + folderSize >= 20720) throw new Error('占用超出上限')
+        else if (uploadList.length >= 5) throw new Error('上传队列已满')
+        ;+size >= 20 && uploadList.push(size)
         return { data: true }
       })
-    },
+    }
   },
   // /delete-file
   {
     method: 'delete',
     path: '/delete-file',
     handler(req, res) {
-      catchError(res, async () => {
-        const hash = req.query.hash as string
-        const { token, filePwd } = req.cookies
-        const path = FileApi.getPath(hash)
-        const admin = await client.get(ADMIN)
-        if (admin === token || path.includes(NOT_SHOW_PASS)) {
-          unlink(path).catch(err => console.warn(err))
-          FileApi.removePath(hash)
-          return { data: null, message: '删除成功' }
-        }
-        else {
-          const regex = /#\d+-/
-          const hasPass = path.match(regex)?.[0]?.length === 15
-          if (!hasPass)
-            return { status: 'Fail', message: '公用文件只允许管理员删除' }
-          const pass = basename(path).split('#')[0]
-          if (filePwd === pass) {
-            unlink(path).catch(err => console.warn(err))
+      catchError(
+        res,
+        async () => {
+          const hash = req.query.hash as string
+          const { token, filePwd } = req.cookies
+          const path = FileApi.getPath(hash)
+          const admin = await client.get(ADMIN)
+          if (admin === token || path.includes(NOT_SHOW_PASS)) {
+            unlink(path).catch((err) => console.warn(err))
             FileApi.removePath(hash)
             return { data: null, message: '删除成功' }
+          } else {
+            const regex = /#\d+-/
+            const hasPass = path.match(regex)?.[0]?.length === 15
+            if (!hasPass) return { status: 'Fail', message: '公用文件只允许管理员删除' }
+            const pass = basename(path).split('#')[0]
+            if (filePwd === pass) {
+              unlink(path).catch((err) => console.warn(err))
+              FileApi.removePath(hash)
+              return { data: null, message: '删除成功' }
+            }
           }
-        }
-        return { status: 'Fail', message: '密码错误' }
-      }, '文件不存在')
-    },
+          return { status: 'Fail', message: '密码错误' }
+        },
+        '文件不存在'
+      )
+    }
   },
   // 通用的文件存储
   {
@@ -167,13 +170,12 @@ $api.add([
           if (err instanceof multer.MulterError) {
             return {
               status: 'Fail',
-              message: err.message,
+              message: err.message
             }
-          }
-          else if (err) {
+          } else if (err) {
             return {
               status: 'Fail',
-              message: err.message,
+              message: err.message
             }
           }
           const data: any[] = []
@@ -181,28 +183,27 @@ $api.add([
             req.files.forEach((f) => {
               data.push({
                 name: f.originalname,
-                url: FileApi.encrypt(f.path),
+                url: FileApi.encrypt(f.path)
               })
               setTimeout(() => {
-                unlink(f.path).catch(err => console.warn(err))
+                unlink(f.path).catch((err) => console.warn(err))
               }, 360000)
             })
-          }
-          else {
+          } else {
             const f = req.file!
             data.push({
               name: f.originalname,
-              url: FileApi.encrypt(f.path),
+              url: FileApi.encrypt(f.path)
             })
             setTimeout(() => {
-              unlink(f.path).catch(err => console.warn(err))
+              unlink(f.path).catch((err) => console.warn(err))
             }, 360000)
           }
           return {
-            data,
+            data
           }
         })
       })
-    },
-  },
+    }
+  }
 ])
